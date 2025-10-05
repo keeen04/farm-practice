088fe6e0aaa13af8ce924992d503f95c9a876ca4
ken0422-admin <reishoku0422@gmail.com>
2025-10-05 19:58:33 +0900

    商品削除画面作成

---
+++ product/pro_delete.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>さかとり南農園</title>
+</head>
+<body>
+
+<?php
+
+try
+{
+
+$pro_code=$_GET['procode'];
+
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+$user ='root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = 'SELECT name FROM mst_product WHERE code=?';
+$stmt = $dbh->prepare($sql);
+$date[]=$pro_code;
+$stmt->execute($date);
+
+$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+$pro_name=$rec['name'];
+
+$dbh = null;
+}
+
+catch(Exception $e)
+{
+    print'ただいま障害により大変ご迷惑をおかけします。';
+    exit();
+}
+
+?>
+
+商品削除<br />
+<br />
+商品コード<br />
+<?php print $pro_code; ?>
+<br />
+商品名<br />
+<?php print $pro_name; ?>
+<br />
+この商品を削除してもよろしいですか？<br />
+<br />
+<form method="post"action="pro_delete_done.php">
+<input type= "hidden" name="code"value="<?php print $pro_code;?>">
+<input type="button"onclick="history.back()"value="戻る">
+<input type="submit"value="OK">
+</form>
