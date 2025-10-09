af94c59de18eea1b8cf8807b18f9be4dc0e25a95
ken0422-admin <reishoku0422@gmail.com>
2025-10-09 21:29:33 +0900

    スタッフのログイン画面を実装しました

---
+++ staff_login/staff_top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>さかとり南農園</title>
+</head>
+<body>
+
+ショップ管理トップメニュー<br/>
+<br/>
+<a href="../staff/staff_list.php">スタッフ管理画面</a><br />
+<br/>
+<a href="../product/pro_list.php">商品管理</a><br />
