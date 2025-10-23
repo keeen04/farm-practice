21618c43c26c6dd9c2f313195efa7236697109e5
ken0422-admin <reishoku0422@gmail.com>
2025-10-23 19:37:37 +0900

    カートの中身がないときに出てくる表示を作りました 同じ商品をカートに入れられないようにしました 多すぎる数量が入らないようにしました
    商品カートを見やすいように変更しました

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -30,10 +30,25 @@ else
 try
 {
 
-$cart =$_SESSION['cart'];
-$kazu =$_SESSION['kazu'];
+if(isset($_SESSION['cart'])==true)
+{
+  $cart =$_SESSION['cart'];
+  $kazu =$_SESSION['kazu'];
+  $max =count($cart);    
+}
+else
+{
+  $max=0;
+}
 
-$max =count($cart);
+if($max==0)
+{
+    print'カートに商品が入っていません。<br />';
+    print'<br />';
+    print'<a href="shop_list.php">商品一覧に戻る</a>';
+    exit();
+}
+
 $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
 $user ='root';
 $password = '';
@@ -76,20 +91,31 @@ catch(Exception $e)
 カートの中身<br />
 <br />
 <form method = "post" action ="kazu_change.php">
+<table border="1">
+<tr>
+<td>商品</td>
+<td>商品画像</td>
+<td>価格</td>
+<td>数量</td>
+<td>小計</td>
+<td>削除</td>
+</tr>
 <?php for($i=0; $i < $max; $i++)
     {
 ?>
+<tr>
 <br>
-      <?php  print $pro_name[$i];?>
-      <?php  print $pro_gazou[$i];?>
-      <?php  print $pro_price[$i];?>円
-      <input type ="text" name="kazu<?php print $i; ?>" value="<?php print $kazu[$i];?>">
-      <?php print $pro_price[$i] * $kazu[$i];?>円
-      <input type ="checkbox"name = "sakujo<?php print $i; ?>">
-      <br />
+     <td> <?php  print $pro_name[$i];?></td>
+      <td><?php  print $pro_gazou[$i];?></td>
+      <td><?php  print $pro_price[$i];?>円</td>
+      <td><input type ="text" name="kazu<?php print $i; ?>" value="<?php print $kazu[$i];?>"></td>
+      <td><?php print $pro_price[$i] * $kazu[$i];?>円</td>
+      <td><input type ="checkbox"name = "sakujo<?php print $i; ?>"></td>
+</tr>
 <?php
     }
 ?>
+</table>
 <input type="hidden" name="max" value="<?php print $max; ?>">
 <input type="submit" value="数量変更"><br />
 <input type="button"onclick="history.back()"value="戻る">
